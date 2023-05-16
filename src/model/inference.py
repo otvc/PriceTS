5e5c2fdabd91e3c8bf4cf081d883c32b471c030b
otvc <olegerko048@gmail.com>
2023-05-16 22:21:52 +0300

    chg output appropriate inference

--- src/model/inference.py
+++ src/model/inference.py
@@ -1,50 +1,57 @@
+import yaml
+import os
 import sys
 
-sys.path.insert(0, './src/')
-
-import os
-
-import yaml
-
+from torch import optim
+from torch.utils.data import DataLoader, Dataset
+from torch import nn
+import torch
 import pandas as pd
 
-import torch
-from torch import nn
-from torch import optim 
-from torch.utils.data import DataLoader, Dataset
+from models import CatEmbLSTM
+from training_instruments import train_CatEmbLSTM, inference_CatEmbLSTM
+
+sys.path.insert(0, 'src/')
 
 from data.prepare_data import MilkTSDataset
 
-from training_instruments import train_CatEmbLSTM, inference_CatEmbLSTM
-
-from models import CatEmbLSTM
 
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
-def load_config_params(path_to_config = 'config/inference_CatEmbLSTM.yaml'):
+
+def load_config_params(path_to_config='config/inference_CatEmbLSTM.yaml'):
     with open(path_to_config, 'r') as f:
         params = yaml.load(f)
     print(params)
     return params
 
-def load_dataset(path_to_inference):
+
+def load_dataset(path_to_inference) -> Dataset:
     inference_dataset = torch.load(path_to_inference)
     return inference_dataset
+
 
 def choose_model(params):
     model = torch.load(params['model']['path'])
     return model
 
+
 if __name__ == '__main__':
     params = load_config_params()
 
-    batch_size = params['dataset']['batch_size']#256
+    batch_size = params['dataset']['batch_size']  # 256
     inference_dataset = load_dataset(params['dataset']['path_to_input'])
-    inference_dataloader = DataLoader(inference_dataset,  batch_size = batch_size, num_workers = params['dataset']['num_workers'])
-
     model = choose_model(params)
-    predicted_values = inference_CatEmbLSTM(model, inference_dataloader)
-    
-    output = pd.DataFrame({params['dataset']['target']:
-                           torch.cat(predicted_values[0]).detach().cpu().numpy()})
+    output = None
+    for part_dataset in inference_dataset.datasets:
+        inference_dataloader = DataLoader(part_dataset,
+                                          batch_size=batch_size,
+                                          num_workers=params['dataset']['num_workers'])
+        predicted_values = inference_CatEmbLSTM(model, inference_dataloader)
+        print(part_dataset)
+        part_dataset.data['predicted'] = None
+        part_dataset.iloc[params['dataset']['lag']:,
+                          'predicted'] = torch.cat(predicted_values[0])
+        output = pd.concat([output, part_dataset])
+
     output.to_csv(params['dataset']['path_to_output'])
